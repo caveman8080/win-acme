ead842befafa4bd0b8be8712ea4682960bece3c3
Caveman8080 <36832235+Caveman8080@users.noreply.github.com>
2026-02-09 17:14:34 -0500

    Fix SYSLIB0051 build warnings by removing obsolete Exception constructors
    Remove [Serializable] attributes and obsolete constructors from custom exception classes in fluent-command-line-parser to eliminate SYSLIB0051 warnings. The obsolete constructors for binary serialization are not used in the codebase, so removal maintains functionality while following modern .NET best practices.

--- src/fluent-command-line-parser/FluentCommandLineParser/CommandNotFoundException.cs
+++ src/fluent-command-line-parser/FluentCommandLineParser/CommandNotFoundException.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Fclp
 {
     /// <summary>
     /// Represents an error that has occurred because an expected command was not found in the parser.
     /// </summary>
-    [Serializable]
     public class CommandNotFoundException : Exception
     {
         /// <summary>
@@ -19,21 +17,5 @@ namespace Fclp
         /// </summary>
         /// <param name="commandName"></param>
         public CommandNotFoundException(string commandName) : base("Expected command " + commandName + " was not found in the parser.") { }
-
-        /// <summary>
-        /// Initialises a new instance of the <see cref="CommandNotFoundException"/> class.
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
-        public CommandNotFoundException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
-
-        /// <summary>
-        /// Initialises a new instance of the <see cref="CommandNotFoundException"/> class.
-        /// </summary>
-        /// <param name="optionName"></param>
-        /// <param name="innerException"></param>
-        public CommandNotFoundException(string optionName, Exception innerException)
-            : base(optionName, innerException) { }
     }
 }
--- src/fluent-command-line-parser/FluentCommandLineParser/OptionAlreadyExistsException.cs
+++ src/fluent-command-line-parser/FluentCommandLineParser/OptionAlreadyExistsException.cs
@@ -23,14 +23,12 @@
 #endregion
 
 using System;
-using System.Runtime.Serialization;
 
 namespace Fclp
 {
     /// <summary>
     /// Represents an error that has occurred because a matching Option already exists in the parser.
     /// </summary>
-    [Serializable]
     public class OptionAlreadyExistsException : Exception
     {
         /// <summary>
@@ -43,22 +41,6 @@ namespace Fclp
         /// </summary>
         /// <param name="optionName"></param>
         public OptionAlreadyExistsException(string optionName) : base(optionName) { }
-
-        /// <summary>
-        /// Initialises a new instance of the <see cref="OptionAlreadyExistsException"/> class.
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="context"></param>
-        public OptionAlreadyExistsException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
-
-        /// <summary>
-        /// Initialises a new instance of the <see cref="OptionAlreadyExistsException"/> class.
-        /// </summary>
-        /// <param name="optionName"></param>
-        /// <param name="innerException"></param>
-        public OptionAlreadyExistsException(string optionName, Exception innerException)
-            : base(optionName, innerException) { }
 
     }
 }
